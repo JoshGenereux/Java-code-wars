6346a5c6d9874c06ca9f9ca8731908c50c27e756
JoshGenereux <josh.genereux13@gmail.com>
2022-01-17 08:22:12 -0700

    finished Ascii

---
+++ src/CodeWars/EightKyu/Ascii.java
@@ -0,0 +1,13 @@
+package CodeWars.EightKyu;
+
+public class Ascii {
+
+    public static void main(String[] args) {
+        System.out.println(getChar(101));
+
+    }
+
+    public static char getChar(int c){
+        return (char)c;
+    }
+}
